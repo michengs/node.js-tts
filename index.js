9de397e026c481a15d02fd3bec3b52ae2e8999bd
mc <46639858+michengs@users.noreply.github.com>
2019-02-10 20:59:07 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ var path = require('path');
 var voice = exports;
 const { exec } = require('child_process');
 const iconv = require('./gbk');
-voice.speak = function(message) {
- 	exec(`powershell.exe Add-Type -AssemblyName System.speech; $speak = New-Object System.Speech.Synthesis.SpeechSynthesizer; $speak.Speak([Console]::In.ReadToEnd()); exit`).stdin.end(iconv.encode(message, 'gbk'));
+voice.speak = function(message,rate) {
+ 	exec(`powershell.exe Add-Type -AssemblyName System.speech; $speak = New-Object System.Speech.Synthesis.SpeechSynthesizer; $speak.Rate =${rate} ; $speak.Speak([Console]::In.ReadToEnd()); exit`).stdin.end(iconv.encode(message, 'gbk'));
 };
